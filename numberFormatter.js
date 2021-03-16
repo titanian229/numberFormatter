20c48a343bedd2cad932a5bf5dc3aa4f2fadd99a
James Lee <jlee@tvo.org>
2021-03-16 12:45:06 -0400

    Initialized files

---
+++ numberFormatter.js
@@ -0,0 +1,7 @@
+// const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+async function mainApp() {
+    console.log("Welcome to the Number Formatter, if this is your first time using it please note:\n\nThis will REPLACE the existing file.  It will also write a file with a line by line description of what was changed and the line numbers.  It is highly recommended to have committed and pushed recently in case you wish to revert these changes.")
+}
