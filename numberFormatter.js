b997279a3051a3dfd6d2c2b96db762b4d2d0f57b
James Lee <jlee@tvo.org>
2021-03-17 16:04:38 -0400

    Updated formatter with antipattern and better backup feature, as well as avoiding content inside math tags

--- numberFormatter.js
+++ numberFormatter.js
@@ -3,16 +3,22 @@ const path = require('path');
 const fs = require('fs');
 
 const BACKUP_DIR = path.resolve(__dirname, 'backups');
-// const outputPath = path.join(OUTPUT_DIR, "output.html");
 
 const locationChecker = (location) => {
     //Check it's an HTML file, and check it's a valid path
-    return true;
+    let locationParsed;
+    locationParsed = path.parse(path.normalize(location.replace(/\"/g, '')));
+
+    if (locationParsed.ext.includes('.html')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 async function mainApp() {
     console.log(
-        "Welcome to the Number Formatter.\n\nIf this is your first time using it please note:\nThis will backup the file to the backups directory within this repo, and overwrite the HTML file you've named.\nIt will also confirm line by line each correction, press enter to confirm and type any characters and press enter to skip.\n\nPress Control-C at any point to exit.\n\n"
+        "Welcome to the Number Formatter.\n\nIf this is your first time using it please note:\nThis will backup the file to the backups directory within this repo, and overwrite the HTML file you've named.\nIt will also confirm line by line each correction, press enter to confirm and type any characters and press enter to skip.\nIf you experience any bugs or have suggestions please email me (James Lee).\nPress Control-C at any point to exit.\n\n"
     );
 
     const menuAction = await inquirer.prompt([
@@ -27,29 +33,26 @@ async function mainApp() {
         },
     ]);
 
-    let fileLocation = menuAction.fileLocation
-    fileLocation = fileLocation.replace(/\"/g, '') //removes quotation marks
+    let fileLocation = menuAction.fileLocation;
+    fileLocation = fileLocation.replace(/\"/g, ''); //removes quotation marks
     // let fileLocation = 'C:\\MAMP\\htdocs\\baf3m_html\\lessons\\baf3m_u2la6.html';
 
     fileLocation = path.normalize(fileLocation);
     let fileContents = fs.readFileSync(fileLocation, 'utf8');
-    //WRITE THE BACKUP
-    
-    
-    
     fileContents = fileContents.split(/\r?\n/);
-    
     newFileContents = [];
 
     let enteredHTMLBody = false;
+    let enteredSkipRegion = false;
+    let foundMatch = false;
 
     for (let j = 0; j < fileContents.length; j++) {
         const line = fileContents[j];
-        //Checking if not yet inside the body tag
-        if (line.includes('<body>')) {
+        //Checking if not yet inside the body tag, to avoid parsing CSS rules
+        if (line.toLowerCase().includes('<body')) {
             enteredHTMLBody = true;
         }
-        if (line.includes('</body>')) {
+        if (line.toLowerCase().includes('</body')) {
             enteredHTMLBody = false;
         }
 
@@ -57,25 +60,41 @@ async function mainApp() {
             newFileContents.push(line);
             continue;
         }
+        //Skipped math/style sections, also skipping only a single line if the closing tag is present
+        if (line.toLowerCase().includes('<math') || line.toLowerCase().includes('<style')) {
+            if (line.toLowerCase().includes('</math') || line.toLowerCase().includes('</style')) {
+                newFileContents.push(line);
+                continue;
+            }
+            enteredSkipRegion = true;
+        }
+        if (line.toLowerCase().includes('</math') || line.toLowerCase().includes('</style')) {
+            enteredSkipRegion = false;
+        }
+
+        if (enteredSkipRegion === true) {
+            newFileContents.push(line);
+            continue;
+        }
 
         let replacementLine = line;
 
         //Anti patterns for BAF3M
-        if (line.toLowerCase().includes('invoice')){
+        if (line.toLowerCase().includes('invoice') || line.toLowerCase().matches(/cheque \d\d\d/g)) {
             newFileContents.push(line);
-            continue
+            continue;
         }
 
         if (line.search(/(\d{4,})|(\d+ \d{3} \d{3})|(\d+ \d{3})/g) !== -1) {
-            //note SIMPLIFY THAT REGEX, THIS IS FOR TESTING
+            //note SIMPLIFY THIS REGEX, THIS IS FOR TESTING
             console.log(line.trim());
             // const matches = Array.from(line.matchAll(/(\d{4,})|(\d+ \d{3} \d{3})|(\d+ \d{3})/g));
-
 
             const matches = line.match(/(\d{4,})|(\d+ \d{3} \d{3})|(\d+ \d{3})/g);
 
             for (let index = 0; index < matches.length; index++) {
                 const match = matches[index];
+                if (!foundMatch) foundMatch = true;
                 // CASE Number with spaces present, changing to nbsp
                 let replacementNumber = match.replace(' ', '&nbsp;');
                 // CASE Number without spaces, adding nbsp
@@ -93,29 +112,35 @@ async function mainApp() {
         newFileContents.push(replacementLine);
     }
 
+    //If no matches in entire file found
+    if (!foundMatch) {
+        console.log(
+            "It looks like there are no numbers in need of formatting in your file, or they're hiding next to some antipatterns."
+        );
+        return;
+    }
+
     // Final confirmation
     const finalConfirm = await inquirer.prompt({
-                type: 'confirm',
-                name: 'confirmation',
-                message:
-                    'Confirm overwriting your file?',
-            })
+        type: 'confirm',
+        name: 'confirmation',
+        message: 'Confirm overwriting your file?',
+    });
 
-    if (!finalConfirm.confirmation){
-        return
+    if (!finalConfirm.confirmation) {
+        return;
     }
 
     //Writing backup
-    fs.writeFileSync(path.join(BACKUP_DIR, path.parse(fileLocation).name + '.html'), fileContents.join('\n\r'));
-    
+    if (!fs.existsSync(BACKUP_DIR)) {
+        fs.mkdirSync(BACKUP_DIR);
+    }
+    fs.copyFileSync(fileLocation, path.join(BACKUP_DIR, path.parse(fileLocation).name + '.html'));
+    // fs.writeFileSync(path.join(BACKUP_DIR, path.parse(fileLocation).name + '.html'), fileContents.join('\n\r'));
+
     fs.writeFileSync(fileLocation, newFileContents.join('\n'));
 
-    console.log("IT HAS BEEN DONE.")
-    
-    
-
-
+    console.log('IT HAS BEEN DONE.');
 }
-
 
 mainApp();
